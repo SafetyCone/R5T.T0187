9de69f72a59a6be11b04f390348fd5bf575f7ec0
davidcoats <david@getsafetycone.com>
2024-02-03 18:24:09 -0800

    Interim changes.

--- source/R5T.T0187/Code/_Types/Interfaces/IHasProjectName.cs
+++ source/R5T.T0187/Code/_Types/Interfaces/IHasProjectName.cs
@@ -5,6 +5,12 @@ using R5T.T0142;
 
 namespace R5T.T0187
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// For stringly-typed complementary type, see R5T.L0096.T000.
+    /// </remarks>
     [DataTypeMarker]
     public interface IHasProjectName
     {
